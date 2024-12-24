56ae0c67f8af8338bdb712a1eaabade97131ae74
Mbuso Cosby <mbusocosby93@gmail.com>
2024-12-24 18:47:49 +0200

    java Script update

---
+++ js/custom.js
@@ -0,0 +1,46 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+      $('.hero-slides').vegas({
+          slides: [
+              { src: 'images/slides/53878213140_93cd6217f9_o.jpg' },
+              { src: 'images/photo_2024-12-16_02-57-41.jpg' },
+              { src: 'images/photo_2024-12-16_02-57-57.jpg' },
+              { src: 'images/54029159203_e99c178b95_o.jpg' }
+          ],
+          timer: false,
+          animation: 'kenburns',
+      });
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height() + 60;
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
